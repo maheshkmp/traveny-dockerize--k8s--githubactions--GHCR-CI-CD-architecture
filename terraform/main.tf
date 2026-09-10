8fc4925c6ed887f8017abb2d3c5fe110a2087347
Mahesh Perera <maheshpererakm@gmail.com>
2026-09-10 17:56:10 +0530

    chore: update SSH ingress rule to allow global access

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,7 +88,7 @@ resource "aws_security_group" "k8s" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.my_ip}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
